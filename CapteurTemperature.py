4d24c012c1e9681f26f11fede9b82b0ffbfefba1
LaurenUnq <44164676+LaurenUnq@users.noreply.github.com>
2018-11-05 16:22:10 +0100

    Add files via upload

---
+++ CapteurTemperature.py
@@ -0,0 +1,14 @@
+#Import Buzzer
+#Import EcranLCD
+#Import Internet
+#import LED
+#Import Bouton
+
+def rappelH():
+
+def mesurerH(temp):
+
+def alerteProcheH():
+
+def alerterSecoursH():
+
---
+++ FrequenceCardiaque.py
@@ -0,0 +1,15 @@
+#import Internet
+#import LED
+#import bouton
+#Import Buzzer
+#Import EcranLCD
+
+def mesurerFC(pouls):
+
+def rappelFC():
+
+def alerteProcheFC():
+
+def alerterSecoursFC():
+
+def sauvegarderFC(): #Mettre dans la bd

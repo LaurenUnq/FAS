59268f6e123ff7fe9f25d111df05c447878bac0d
LaurenUnq <44164676+LaurenUnq@users.noreply.github.com>
2018-11-11 15:28:50 +0100

    Update lcd.py
    Ecriture des fonction avec la librairie  RPLCD

--- lcd.py
+++ lcd.py
@@ -1,7 +1,16 @@
 #ajouter les imports nécessaires
+#sudo pip install RPLCD
+from RPLCD import CharLCD
+import time
 
-# effecer l'ecran
+# effacer l'ecran
 def clearEcran():
+  lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
+  lcd.write_string(u'Espace RaspberryFrancais')
+  time.sleep(5)
+  lcd.clear()
 
 #ecrire sur l'ecran
 def ecrireMessage(texte):
+  lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
+  lcd.write_string(texte)

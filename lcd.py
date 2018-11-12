f9b8165cbe2574e7a795142e1c6a0f8023e69173
LaurenUnq <44164676+LaurenUnq@users.noreply.github.com>
2018-11-12 20:12:40 +0100

    Update lcd.py
    Corrections de fonction

--- lcd.py
+++ lcd.py
@@ -6,8 +6,6 @@ import time
 # effacer l'ecran
 def clearEcran():
   lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
-  lcd.write_string(u'Espace RaspberryFrancais')
-  time.sleep(5)
   lcd.clear()
 
 #ecrire sur l'ecran

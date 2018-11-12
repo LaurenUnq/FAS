7789adbfe0850367e0d1d841acb697661b52eb1f
LaurenUnq <44164676+LaurenUnq@users.noreply.github.com>
2018-11-12 20:10:37 +0100

    Update led.py
    Ajout des fonctions d'une led

--- led.py
+++ led.py
@@ -1,7 +1,22 @@
 #ajouter les imports nécesssaires
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.OUT)
 
-#allumer une led
+#broche21
+#allumer une LED
 def allumerLED():
+  GPIO.output(21, GPIO.HIGH)
+  
+#faire clignoter une LED
+def clignoterLED():
+  while True :
+    GPIO.output(25, GPIO.HIGH)
+    time.sleep(1) 
+    GPIO.output(25, GPIO.LOW) 
+    time.sleep(1)
 
 #eteindre la LED
 def eteindreLED():
+    GPIO.output(25 GPIO.LOW)

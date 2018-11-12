14b1dfbb25c47400ed5cad894072dd5903418230
LaurenUnq <44164676+LaurenUnq@users.noreply.github.com>
2018-11-12 20:27:27 +0100

    Update buzzer.py
    Ajout des fonctions du buzzer

--- buzzer.py
+++ buzzer.py
@@ -1,4 +1,20 @@
 #ajouter les imports
+from gpiozero import Buzzer
+from time import sleep
+
+#s'il est connecté au port 17
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+buzzer = Buzzer(17)
 
 #Fonction qui acive le buzzer avec une intensité précise
-def buzzer(intensite):
+def buzzer(intensite):)
+  while True:
+    GPIO.output(buzzer,GPIO.HIGH)
+    sleep(1)
+    GPIO.output(buzzer,GPIO.LOW)
+    sleep(1)
+
+def arretBuzzer():
+  GPIO.output(buzzer,GPIO.LOW)
+  GPIO.cleanup()

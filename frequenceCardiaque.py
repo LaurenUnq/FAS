5d03f045ac3456b70377ec4986e8070b856f51bb
LaurenUnq <44164676+LaurenUnq@users.noreply.github.com>
2018-11-15 08:17:35 +0100

    Update frequenceCardiaque.py
    Complétion des fonction pour la fréquence cardiaque

--- frequenceCardiaque.py
+++ frequenceCardiaque.py
@@ -13,10 +13,20 @@ def initialiserFC(pin):
 def mesurerFC():
 
 def rappelFC():
+  #ecrireMessage("Rappel : prendre FC !")      #utilisation de la fonction depuis lcd
+  #buzzer()     #utilisation de la fonction depuis buzzer
+  #clignoterLED()      #utilisation de la fonction depuis led
+  #time.sleep(10)
+  #clearEcran()      #utilisation de la fonction depuis lcd
+  #arretBuzzer()     #utilisation de la fonction depuis buzzer
 
 #envoyer un mail au proche
 def alerteProcheFC():
-
+  #msg="ALERTE ! Il semblerait que Mr ou MMe X rencontre des difficultés au niveau de la fréquence cardiaque ..."
+  #if (temp >= 25):
+    #alerterProche(msg)
+    
+   
 #def alerterSecoursFC():
 
 def sauvegarderFC(): #Mettre dans la bd

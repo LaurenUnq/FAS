0f493c305e09b313e4c48d0df1f7a4790e3a630a
LaurenUnq <44164676+LaurenUnq@users.noreply.github.com>
2018-11-16 08:36:36 +0100

    Update frequenceCardiaque.py
    Mesurer la fréquence cardiaque

--- frequenceCardiaque.py
+++ frequenceCardiaque.py
@@ -4,13 +4,31 @@
 #Import Buzzer
 #Import EcranLCD
 import RPi.GPIO as GPIO
+from grovepi import *
 import time
 
 # Initialiser
 def initialiserFC(pin):
   GPIO.setup(pin,GPIO.IN)
 
-def mesurerFC():
+#Le capteur renvoie0 quand il sent pas de batement, et 1 quand il y en a
+def mesurerFC(pin,time): #pin est le pin sur lequel est le capteur, time et le temps pendant lequel on prend la fréquence (ex:20sec)
+	pinMode(sens,"INPUT") #plus le temps time est grand plus ce sera précis
+	current = time.time()
+	prec = 0
+	count = 0
+	sec = current
+	while sec < current + timeInt  :
+		lec = digitalRead(sens)
+		if not prec and lec :
+			count += 1
+		prec = lec
+
+		sec = time.time()
+
+	btm = count * (60/timeInt)
+
+	return btm
 
 def rappelFC():
   #ecrireMessage("Rappel : prendre FC !")      #utilisation de la fonction depuis lcd

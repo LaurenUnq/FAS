9d123e2da9a88db183d24e3472c934a30a5fe803
LaurenUnq <44164676+LaurenUnq@users.noreply.github.com>
2018-12-15 18:12:17 +0100

    Update main.py
    finition du main

--- main.py
+++ main.py
@@ -12,12 +12,25 @@ while true :
     f = rappelFC()
     f.start()
     f.join()
-  
-	e = grovepi.digitalRead(button1)
-	if e == 1:
-  	self._return = True
-		time.sleep(0.5)
+  compteur = 1
+  e = grovepi.digitalRead(button1)
+  if e == 1:
+    self._return = True
     mesurerFC(2,20)
+    compteur = 0
+    time.sleep(0.5)
+  else :
+    time.sleep(900) #on attends 15 min avant de relancer un rappel
+    f = rappelFC()
+    f.start()
+    f.join()
+    compteur = compteur +1
+
+  if compteur == 4 :
+    compteur = 0
+    m=message.MailProches("Ce message vous est envoyé car votre proche n'a pas confirmé sa prise d efréquence cardiaque")
+    m.start()
+    m.join()
  
   mesurerH()
 

0f42d08da94d3ed90a9812a3bb6111554586407b
LaurenUnq <44164676+LaurenUnq@users.noreply.github.com>
2018-12-15 17:48:10 +0100

    Update main.py
    remplissage du main : Rappel de fréquence cardiaque tous les jours à 8h / mesure la fréquence cardiaque si l'utilisateur appuie sur le bouton / Rappel de prise de verre d'eau

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 #définir les imports..
+import time
+import frequenceCardiaque
+import capteurTemperature
+import buzzer
+import led
+import lcd
+import message
+
+while true :
+  if time.strftime('%H')==8 :
+    f = rappelFC()
+    f.start()
+    f.join()
+  
+	e = grovepi.digitalRead(button1)
+	if e == 1:
+  	self._return = True
+		time.sleep(0.5)
+    mesurerFC(2,20)
+ 
+  mesurerH()
 
